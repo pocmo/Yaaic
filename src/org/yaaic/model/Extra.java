db06f5450dcb31328dfe278a9ace752e02a71524
Sebastian Kaspari <s.kaspari@googlemail.com>
2010-03-24 19:26:38 +0100

    New class for bundle params (org.yaaic.model.Extra)

---
+++ src/org/yaaic/model/Extra.java
@@ -0,0 +1,32 @@
+/*
+Yaaic - Yet Another Android IRC Client
+
+Copyright 2009-2010 Sebastian Kaspari
+
+This file is part of Yaaic.
+
+Yaaic is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Yaaic is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Yaaic.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package org.yaaic.model;
+
+/**
+ * Helper class for constants used for bundle params (extras)
+ * 
+ * @author Sebastian Kaspari <sebastian@yaaic.org>
+ */
+public class Extra
+{
+	public static final String SERVER 		= "server";
+	public static final String CONVERSATION = "conversation";
+}
