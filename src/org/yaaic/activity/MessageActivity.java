243e38a5e3801e3d3cdc9bde219642dfba97c4b4
Sebastian Kaspari <s.kaspari@googlemail.com>
2010-04-25 15:27:55 +0200

    Added MessageActivity to show single messages and with options

---
+++ src/org/yaaic/activity/MessageActivity.java
@@ -0,0 +1,53 @@
+/*
+Yaaic - Yet Another Android IRC Client
+
+Copyright 2009-2010 Sebastian Kaspari
+
+This file is part of Yaaic.
+
+Yaaic is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Yaaic is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Yaaic.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package org.yaaic.activity;
+
+import org.yaaic.R;
+import org.yaaic.model.Extra;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.TextView;
+
+/**
+ * Activity for single message view
+ * 
+ * @author Sebastian Kaspari <sebastian@yaaic.org>
+ */
+public class MessageActivity extends Activity
+{
+	/**
+	 * On create
+	 */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+    	super.onCreate(savedInstanceState);
+    	
+    	requestWindowFeature(Window.FEATURE_NO_TITLE);
+    	setContentView(R.layout.message);
+    	
+    	((TextView) findViewById(R.id.message)).setText(
+			getIntent().getExtras().getString(Extra.MESSAGE)
+		);
+    }
+}
