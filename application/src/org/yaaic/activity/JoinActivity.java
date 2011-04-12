b3fd4157dc185b228b8cb3c9b2e51a12163f7f3d
Sebastian Kaspari <s.kaspari@googlemail.com>
2011-04-12 23:05:09 +0200

    JoinActivity: Set cursor position

--- application/src/org/yaaic/activity/JoinActivity.java
+++ application/src/org/yaaic/activity/JoinActivity.java
@@ -49,6 +49,8 @@ public class JoinActivity extends Activity implements OnClickListener
         setContentView(R.layout.join);
 
         ((Button) findViewById(R.id.join)).setOnClickListener(this);
+
+        ((EditText) findViewById(R.id.channel)).setSelection(1);
     }
 
     /**
