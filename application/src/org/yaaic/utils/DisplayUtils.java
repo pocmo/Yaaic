9884ef1bff4f3cd0732ab67834dd2a00327e7a05
Sebastian Kaspari <s.kaspari@gmail.com>
2012-07-28 11:29:57 +0200

    DisplayUtils: Helper class for methods regarding the display of the current device.

---
+++ application/src/org/yaaic/utils/DisplayUtils.java
@@ -0,0 +1,60 @@
+/*
+Yaaic - Yet Another Android IRC Client
+
+Copyright 2009-2012 Sebastian Kaspari
+
+This file is part of Yaaic.
+
+Yaaic is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Yaaic is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Yaaic.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.yaaic.utils;
+
+import android.content.Context;
+
+/**
+ * Helper class for methods regarding the display of the current device.
+ *
+ * @author Sebastian Kaspari <sebastian@yaaic.org>
+ */
+public class DisplayUtils
+{
+    private static float density = -1;
+
+    /**
+     * Convert the given density-independent pixels into real pixels for the
+     * display of the device.
+     *
+     * @param dp
+     * @return
+     */
+    public static int convertToPixels(Context context, int dp) {
+        float density = getScreenDensity(context);
+
+        return (int) (dp * density + 0.5f);
+    }
+
+    /**
+     * Get the density of the display of the device.
+     *
+     * @param context
+     * @return
+     */
+    public static float getScreenDensity(Context context) {
+        if (density == -1) {
+            density = context.getResources().getDisplayMetrics().density;
+        }
+
+        return density;
+    }
+}
