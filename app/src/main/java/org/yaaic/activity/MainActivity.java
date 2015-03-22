33560b49131c0d7bebf4c30d94bc7da91e427920
Sebastian Kaspari <s.kaspari@gmail.com>
2015-03-22 21:40:41 +0100

    MainActivity: Whoops. Remove space in package name.

--- app/src/main/java/org/yaaic/activity/MainActivity.java
+++ app/src/main/java/org/yaaic/activity/MainActivity.java
@@ -18,7 +18,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with Yaaic.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.yaaic.a ctivity;
+package org.yaaic.activity;
 
 import android.content.ComponentName;
 import android.content.Intent;
